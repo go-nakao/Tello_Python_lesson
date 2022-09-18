4d9b5f256c94b23ac81cb885bbe336485b0603b5
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-18 19:12:49 +0900

    address整理

--- Tello3.py
+++ Tello3.py
@@ -1,76 +1,69 @@
-#
-# Tello Python3 Control Demo 
-#
-# http://www.ryzerobotics.com/
-#
-# 1/1/2018
-
-import threading 
+import threading
 import socket
-import sys
+# import sys
 import time
-import platform  
+import platform
 
-host = ''
-port = 9000
-locaddr = (host,port) 
 
+# TelloのプライベートIPとポート番号
+tello_ip = '192.168.10.1'
+tello_port = 8889
+tello_address = (tello_ip, tello_port)
+
+# hostのプライベートIPとポート番号
+host_ip = '192.168.10.2'
+host_port = 8889
+host_address = (host_ip, host_port)
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(host_address)
 
-tello_address = ('192.168.10.1', 8889)
-
-sock.bind(locaddr)
 
 def recv():
     count = 0
-    while True: 
+    while True:
         try:
             data, server = sock.recvfrom(1518)
             print(data.decode(encoding="utf-8"))
         except Exception:
-            print ('\nExit . . .\n')
+            print('\nExit . . .\n')
             break
 
 
-print ('\r\n\r\nTello Python3 Demo.\r\n')
+print('\r\n\r\nTello Python3 Demo.\r\n')
 
-print ('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
+print('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
 
-print ('end -- quit demo.\r\n')
+print('end -- quit demo.\r\n')
 
 
-#recvThread create
+# recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
-while True: 
+while True:
     try:
         python_version = str(platform.python_version())
-        version_init_num = int(python_version.partition('.')[0]) 
+        version_init_num = int(python_version.partition('.')[0])
        # print (version_init_num)
         if version_init_num == 3:
-            msg = input("");
+            msg = input("")
         elif version_init_num == 2:
-            msg = raw_input("");
-        
+            msg = input("")
+
         if not msg:
-            break  
+            break
 
         if 'end' in msg:
-            print ('...')
-            sock.close()  
+            print('...')
+            sock.close()
             break
 
         # Send data
-        msg = msg.encode(encoding="utf-8") 
+        msg = msg.encode(encoding="utf-8")
         sent = sock.sendto(msg, tello_address)
     except KeyboardInterrupt:
-        print ('\n . . .\n')
-        sock.close()  
+        print('\n . . .\n')
+        sock.close()
         break
-
-
-
-
