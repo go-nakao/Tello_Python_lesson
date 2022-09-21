64d647a98f4ac653ca1f357cc9538701eaa7b446
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-22 00:19:43 +0900

    tello ビデオストリーム開始

--- Tello3.py
+++ Tello3.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import cv2
 
 
 # TelloのプライベートIPとポート番号
@@ -16,6 +17,11 @@ host_address = (host_ip, host_port)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(host_address)
 
+# Telloからの映像受信用のローカルIPアドレス、宛先ポート番号
+TELLO_CAMERA_ADDRESS = 'udp://@0.0.0.0:11111'
+
+# キャプチャ用オブジェクト
+cap = None
 
 print('\r\n\r\nTello Python3 Tech Blog.\r\n')
 
@@ -35,16 +41,39 @@ while True:
         print(data.decode(encoding="utf-8"))
         time.sleep(5)
 
+        if cap is None:
+            cap = cv2.VideoCapture(TELLO_CAMERA_ADDRESS)
+
+        if not cap.isOpened():
+            cap.open(TELLO_CAMERA_ADDRESS)
+
+        time.sleep(1)
+
+        ret, frame = cap.read()
+
+        # 動画フレームが空ならスキップ
+        if frame is None or frame.size == 0:
+            continue
+
+        # カメラ映像のサイズを半分にしてウィンドウに表示
+        frame_height, frame_width = frame.shape[:2]
+        frame2 = cv2.resize(frame, (int(frame_width/2), int(frame_height/2)))
+
+        # カメラ映像を画面に表示
+        cv2.imshow('Tello Camera View', frame2)
+
         # 離陸
         sent = sock.sendto('takeoff'.encode(encoding="utf-8"), tello_address)
         print('takeoff')
         data, _ = sock.recvfrom(1518)
         print(data.decode(encoding="utf-8"))
-        time.sleep(10)
+        time.sleep(5)
         # 前方へ移動
         sent = sock.sendto('forward 100'.encode(
             encoding="utf-8"), tello_address)
         print('forward')
+        data, _ = sock.recvfrom(1518)
+        print(data.decode(encoding="utf-8"))
         time.sleep(5)
         # 右旋回
         sent = sock.sendto('cw 90'.encode(encoding="utf-8"), tello_address)
@@ -71,21 +100,22 @@ while True:
         print(data.decode(encoding="utf-8"))
         time.sleep(5)
         # ストリーミング終了
+        cap.release()
+        cv2.destroyAllWindows()
         sent = sock.sendto('streamoff'.encode(encoding="utf-8"), tello_address)
         print('video end')
         data, _ = sock.recvfrom(1518)
         print(data.decode(encoding="utf-8"))
         time.sleep(3)
+
         # 終了
-        msg = 'end'
+        sent = sock.sendto('end'.encode(encoding="utf-8"), tello_address)
         print('end:ok')
         data, _ = sock.recvfrom(1518)
         print(data.decode(encoding="utf-8"))
-
-        if 'end' in msg:  # endが入力されたら、ソケット終了
-            print('Exit...')
-            sock.close()
-            break
+        print('Exit...')
+        sock.close()
+        break
 
     except KeyboardInterrupt:
         print('\n . . .\n')
