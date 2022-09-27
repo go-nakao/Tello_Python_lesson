f3819ead514315686b7e9013c0203a25ca76fe99
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-27 11:29:38 +0900

    修正

--- Tello3_video_flight.py
+++ Tello3_video_flight.py
@@ -4,74 +4,46 @@ import threading
 import cv2
 import numpy as np
 
-# データ受け取り用の関数
-
-
-def udp_receiver():
-    while True:
-        try:
-            response, _ = sock.recvfrom(1024)
-        except Exception as e:
-            print(e)
-            break
-
-# 新規作成コード
+# 飛行用の関数定義
 
 
 def flight():
     while True:
-        try:
-            sent = sock.sendto('command'.encode(
-                encoding="utf-8"), tello_address)
-            print('command')
-            time.sleep(5)
-            sent = sock.sendto('takeoff'.encode(
-                encoding="utf-8"), tello_address)
-            print('takeoff')
-            time.sleep(10)
-            # 前方へ移動
-            sent = sock.sendto('forward 100'.encode(
-                encoding="utf-8"), tello_address)
-            print('forward')
-            time.sleep(5)
-            # 右旋回
-            sent = sock.sendto('cw 90'.encode(encoding="utf-8"), tello_address)
-            print('cw')
-            time.sleep(5)
-            # 上昇
-            sent = sock.sendto('up 50'.encode(encoding="utf-8"), tello_address)
-            print('up')
-            time.sleep(5)
-            # 後退
-            sent = sock.sendto('back 100'.encode(
-                encoding="utf-8"), tello_address)
-            print('back')
-            time.sleep(5)
-            sent = sock.sendto('land'.encode(encoding="utf-8"), tello_address)
-            print('land')
-            time.sleep(5)
-            msg = 'end'
-            print('end:ok')
+        sent = sock.sendto('command'.encode(
+            encoding="utf-8"), tello_address)
+        print('command')
+        time.sleep(5)
+        sent = sock.sendto('takeoff'.encode(
+            encoding="utf-8"), tello_address)
+        print('takeoff')
+        time.sleep(10)
+        # 前方へ移動
+        sent = sock.sendto('forward 100'.encode(
+            encoding="utf-8"), tello_address)
+        print('forward')
+        time.sleep(5)
+        # 右旋回
+        sent = sock.sendto('cw 90'.encode(encoding="utf-8"), tello_address)
+        print('cw')
+        time.sleep(5)
+        # 上昇
+        sent = sock.sendto('up 50'.encode(encoding="utf-8"), tello_address)
+        print('up')
+        time.sleep(5)
+        # 後退
+        sent = sock.sendto('back 100'.encode(
+            encoding="utf-8"), tello_address)
+        print('back')
+        time.sleep(5)
+        sent = sock.sendto('land'.encode(encoding="utf-8"), tello_address)
+        print('land')
+        time.sleep(5)
+        msg = 'end'
+        print('end:ok')
 
-            if 'end' in msg:  # endが入力されたら、ソケット終了
-                print('Exit...')
-                sock.close()
-                break
-
-        except KeyboardInterrupt:
-            print('\n . . .\n')
-            sock.close()  # 通信終了
+        if 'end' in msg:
+            print('Land...')
             break
-
-
-# Telloからの映像受信用のローカルIPアドレス、宛先ポート番号
-TELLO_CAMERA_ADDRESS = 'udp://@0.0.0.0:11111'
-
-# キャプチャ用のオブジェクト
-cap = None
-
-# データ受信用のオブジェクト備
-response = None
 
 
 # TelloのプライベートIPとポート番号
@@ -84,17 +56,20 @@ host_ip = '192.168.10.2'
 host_port = 8889
 host_address = (host_ip, host_port)
 
+# Telloからの映像受信用のローカルIPアドレス、宛先ポート番号
+tello_video__address = 'udp://@0.0.0.0:11111'
+
+# キャプチャ用のオブジェクト
+cap = None
+
 # UDP通信の確立
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(host_address)
 
-# 受信用スレッドの作成
-thread = threading.Thread(target=udp_receiver, args=())
-thread.daemon = True
-thread.start()
-
 # コマンドモード
 sock.sendto('command'.encode('utf-8'), tello_address)
+
+print('\r\n\r\nTello Python3 Tech Blog.\r\n')
 
 time.sleep(1)
 
@@ -104,10 +79,10 @@ sock.sendto('streamon'.encode('utf-8'), tello_address)
 time.sleep(5)
 
 if cap is None:
-    cap = cv2.VideoCapture(TELLO_CAMERA_ADDRESS)
+    cap = cv2.VideoCapture(tello_video__address)
 
 if not cap.isOpened():
-    cap.open(TELLO_CAMERA_ADDRESS)
+    cap.open(tello_video__address)
 
 time.sleep(1)
 
@@ -115,27 +90,27 @@ thread = threading.Thread(target=flight)
 thread.start()
 
 while True:
-    ret, frame = cap.read()
+    try:
+        ret, frame = cap.read()
 
-    # 動画フレームが空ならスキップ
-    if frame is None or frame.size == 0:
-        continue
+        # 動画フレームが空ならスキップ
+        if frame is None or frame.size == 0:
+            continue
 
-    # カメラ映像のサイズを半分にしてウィンドウに表示
-    frame_height, frame_width = frame.shape[:2]
-    frame = cv2.resize(frame, (int(frame_width/2), int(frame_height/2)))
+        # 画面に受信映像を表示
+        cv2.imshow('Tello Flight View', frame)
 
-    cv2.imshow('Tello Camera View', frame)
+        # qキーで終了
+        if cv2.waitKey(1) & 0xFF == ord('q'):
 
-    # qキーで終了
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # 「control + c」で終了する
+    except KeyboardInterrupt:
+        print('\n . . .\n')
+        cap.release()
+        cv2.destroyAllWindows()
 
+        # ビデオストリーミング停止
+        sock.sendto('streamoff'.encode('utf-8'), tello_address)
+        print('streamoff:ok')
         break
-cap.release()
-cv2.destroyAllWindows()
-
-# ビデオストリーミング停止
-sock.sendto('streamoff'.encode('utf-8'), tello_address)
-
-
-print('\r\n\r\nTello Python3 Tech Blog.\r\n')
